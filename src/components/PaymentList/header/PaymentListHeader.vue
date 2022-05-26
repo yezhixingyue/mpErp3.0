6314974a911ac11eab6dac9dd14bf9a577a776a8
FHL <yezhixingyue@163.com>
2022-05-26 15:34:00 +0800

    付款单列表购买状态清空状态不能还原修复

--- src/components/PaymentList/header/PaymentListHeader.vue
+++ src/components/PaymentList/header/PaymentListHeader.vue
@@ -1,7 +1,13 @@
 <template>
   <header class="mp-payment-list-header">
     <div class="first-row">
-      <PayStatusSelect />
+      <RadioButtonGroupComp
+        :radioList="statusEnumList"
+        :requestFunc="getDataList"
+        v-model="Status"
+        :isFull="true"
+        title="购买状态"
+      />
       <div class="user-selector">
         <order-channel-selector
           :options="userTypeList"
@@ -24,7 +30,6 @@
         />
       </div>
       <StaffSelector />
-      <!-- <PaymentMethod /> -->
       <OrderMethod />
     </div>
     <div class="s">
@@ -51,56 +56,50 @@
         @reset='() => this.clearCondition()'
       />
     </div>
-    <!-- <TimeSearchSelector /> -->
   </header>
 </template>
 
 <script>
-// import TimeSearchSelector from '@/components/PaymentList/header/TimeSearchSelector.vue';
-import PayStatusSelect from '@/components/PaymentList/header/PayStatusSelect.vue';
-// import PaymentMethod from '@/components/PaymentList/header/PaymentMethod.vue';
 import StaffSelector from '@/components/PaymentList/header/StaffSelector.vue';
 import OrderMethod from '@/components/PaymentList/header/OrderMethod.vue';
 import OrderChannelSelector from '@/components/common/SelectorComps/OrderChannelSelector.vue';
 import SearchInputComp from '@/components/common/SearchInputComp.vue';
-// import ElDateRangeSelector from '@/components/common/SelectorComps/ElDateRangeSelector';
+import RadioButtonGroupComp from '@/components/common/RadioButtonGroupComp.vue';
 import LineDateSelectorComp from '@/components/common/SelectorComps/LineDateSelectorComp.vue';
 import { mapState, mapMutations } from 'vuex';
 
 export default {
   components: {
-    // TimeSearchSelector,
-    PayStatusSelect,
     StaffSelector,
-    // PaymentMethod,
     OrderMethod,
     OrderChannelSelector,
-    // ElDateRangeSelector,
     SearchInputComp,
     LineDateSelectorComp,
+    RadioButtonGroupComp,
   },
   data() {
     return {
       // eslint-disable-next-line max-len
-      dateList: [{ name: '全部', ID: 'all' }, { name: '今天', ID: 'today' }, { name: '昨天', ID: 'yesterday' }, { name: '本月', ID: 'curMonth' }, { name: '上月', ID: 'lastMonth' }],
+      dateList: [{ name: '不限', ID: 'all' }, { name: '今天', ID: 'today' }, { name: '昨天', ID: 'yesterday' }, { name: '本月', ID: 'curMonth' }, { name: '上月', ID: 'lastMonth' }],
     };
   },
   computed: {
-    ...mapState('common', ['userTypeList', 'userRankList']),
+    ...mapState('common', ['userTypeList', 'userRankList', 'PayStatusList']),
     ...mapState('paymentModule', ['set2PaymentList', 'tableData']),
-    // conditionDate: {
-    //   get() {
-    //     return [this.set2PaymentList.CreateDate.First, this.set2PaymentList.CreateDate.Second];
-    //   },
-    //   set(newVal) {
-    //     const [key, value] = newVal?.length === 2 ? newVal : ['', ''];
-    //     this.setDataListRequestObj([['CreateDate', 'First'], key]);
-    //     this.setDataListRequestObj([['CreateDate', 'Second'], value]);
-    //     this.getDataList();
-    //   },
-    // },
+    statusEnumList() {
+      const list = this.PayStatusList.map(it => ({ ID: it.ID, name: it.Name }));
+      return [{ ID: '', name: '不限' }, ...list];
+    },
     UserDefinedTimeIsActive() {
       return this.set2PaymentList.DateType === '' && !!this.set2PaymentList.CreateDate.First && !!this.set2PaymentList.CreateDate.Second;
+    },
+    Status: {
+      get() {
+        return this.set2PaymentList.Status;
+      },
+      set(newVal) {
+        this.setDataListRequestObj([['Status', ''], newVal]);
+      },
     },
   },
   methods: {
@@ -124,6 +123,9 @@ export default {
     > .user-selector {
       display: flex;
       margin-right: 80px !important;
+    }
+    > .mp-common-comps-radio-group-wrap {
+      margin-right: 8px;
     }
   }
   > .s {
