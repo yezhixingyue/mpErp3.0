d5bdad89d6edfc96885d4e9c45c556253dd41879
FHL <yezhixingyue@163.com>
2023-06-07 14:21:45 +0800

    作业次数添加缺省文字说明

--- src/components/common/Layout/HeaderTabs.vue
+++ src/components/common/Layout/HeaderTabs.vue
@@ -221,7 +221,7 @@ export default {
     onDocClick(target) {
       if (!this.Permission) return;
       const siteType = 1;
-      window.open(`http://127.0.0.1:5173/init?token=${this.Permission.Token}&siteType=${siteType}&target=${target}`);
+      window.open(`http://192.168.1.92:3020/init?token=${this.Permission.Token}&siteType=${siteType}&target=${target}`);
     },
     handleLogoutClick() { // 退出
       this.messageBox.warnCancelNullMsg('确定退出登录吗?', () => {
