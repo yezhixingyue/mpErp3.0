6df87ce7d02b2a46bc79d0592558b97b4f22c619
FHL <yezhixingyue@163.com>
2023-10-26 11:03:08 +0800

    添加转换异常列表、订单详情添加工厂进度展示（暂未开放）

--- src/components/order/DialogContent/OrderDetail.vue
+++ src/components/order/DialogContent/OrderDetail.vue
@@ -371,11 +371,11 @@ export default {
       return '';
     },
     showProdProgress() {
-      if (this.$route.name === 'orderManage' && this.OrderStatusList.filter(it => it.canProdProgress).map(it => it.ID).includes(this.showData.Status)) {
-        const row = this.orderListData.find(it => it.OrderID === this.showData.OrderID);
-        console.log('showProdProgress', row);
-        return row?.IsAutoConvert;
-      }
+      // if (this.$route.name === 'orderManage' && this.OrderStatusList.filter(it => it.canProdProgress).map(it => it.ID).includes(this.showData.Status)) {
+      //   const row = this.orderListData.find(it => it.OrderID === this.showData.OrderID);
+      //   console.log('showProdProgress', row);
+      //   return row?.IsAutoConvert;
+      // }
       return false;
     },
   },
