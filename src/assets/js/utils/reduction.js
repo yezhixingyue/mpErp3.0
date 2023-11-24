cf97c673aabd2d37c1a10a146c7509d17e9d6620
FHL <yezhixingyue@163.com>
2023-11-24 09:47:21 +0800

    Bug Fixed

--- src/assets/js/utils/reduction.js
+++ src/assets/js/utils/reduction.js
@@ -3,17 +3,18 @@
  *
  * @param {Object} obj 需要改的对象
  * @param {Object} data 原有数据
+ * @param {Boolean} force 是否强制赋值
  */
-const restoreInitDataByOrigin = (obj, data) => {
+const restoreInitDataByOrigin = (obj, data, force = false) => {
   if (data && Object.prototype.toString.call(data) === '[object Object]' && obj && Object.prototype.toString.call(obj) === '[object Object]') {
     const _obj = obj;
     Object.keys(data).forEach(key => {
       if (Object.prototype.hasOwnProperty.call(_obj, key)) {
         if (data[key] && Object.prototype.toString.call(data[key]) === '[object Object]') {
-          restoreInitDataByOrigin(obj[key], data[key]);
+          restoreInitDataByOrigin(obj[key], data[key], force);
         } else if (Array.isArray(data[key])) {
           _obj[key] = JSON.parse(JSON.stringify(data[key]));
-        } else if (data[key] || data[key] === 0 || data[key] === false) {
+        } else if (data[key] || data[key] === 0 || data[key] === false || force) {
           _obj[key] = data[key];
         }
       }
