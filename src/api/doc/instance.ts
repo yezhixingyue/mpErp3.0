2d5fd60edcee235491660d9972b39ba9ed14bed5
FHL <yezhixingyue@163.com>
2024-05-07 18:20:46 +0800

    文档地址更改

--- src/api/doc/instance.ts
+++ src/api/doc/instance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { IMPAxiosInstance } from '../types/type';
 import { getInstanceWithIntercept } from '../getInstanceWithIntercept';
 
-export const docBaseURL = 'http://192.168.2.214:8002';
+export const docBaseURL = 'http://192.168.1.92:8090';
 
 export const docClientURL = process.env.NODE_ENV === 'development' ? 'http://192.168.2.237:5173/doc' : 'http://192.168.1.92:8060/openPlatform/doc';
 
