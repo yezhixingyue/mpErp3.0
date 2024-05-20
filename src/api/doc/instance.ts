93f20aaaa843696727b658009be553d2d2e0042b
FHL <yezhixingyue@163.com>
2024-05-20 15:29:48 +0800

    8060切换至8050

--- src/api/doc/instance.ts
+++ src/api/doc/instance.ts
@@ -4,7 +4,7 @@ import { getInstanceWithIntercept } from '../getInstanceWithIntercept';
 
 export const docBaseURL = 'http://192.168.1.92:8090';
 
-export const docClientURL = process.env.NODE_ENV === 'development' ? 'http://192.168.2.237:5173/doc' : 'http://192.168.1.92:8060/openPlatform/doc';
+export const docClientURL = process.env.NODE_ENV === 'development' ? 'http://192.168.2.237:5173/doc' : 'http://192.168.1.92:8050/openPlatform/doc';
 
 const instance: IMPAxiosInstance = axios.create({ baseURL: docBaseURL });
 
