c3f89593bfd3a99f453c3020fcf52caf2f1a8cf0
lihaoyang <l15639757696@163.com>
2024-06-14 14:29:52 +0800

    nothing

---
+++ src/components/AfterSalesComps/ResponsibilityDifferentiationComp.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="responsibility-differentiation-comp">
+    <header class="title is-bold">划分责任</header>
+    <ul>
+      <li>
+        <span class="label is-bold">问题类别：</span><span class="value">湿货、生产质量问题、材料质量问题、晚货</span>
+      </li>
+      <li>
+        <span class="label is-bold">责任占比：</span><span class="value">订单详情</span>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  props: {
+    condition: {
+      type: Object,
+      default: () => ({}),
+    },
+    dataList: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+
+  components: {
+  },
+  computed: {
+    ...mapState('common', ['userTypeList']),
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<style lang='scss'>
+.responsibility-differentiation-comp {
+  padding-left: 20px;
+  min-width: 498px;
+  width: 498px;
+  box-sizing: border-box;
+  padding-right: 30px;
+  >.title{
+    font-size: 16px;
+    line-height: 20px;
+    margin-top: 10px;
+    position: relative;
+    margin-bottom: 20px;
+    &::before{
+      content: "";
+      display: inline-block;
+      height: 14px;
+      width: 3px;
+      position: absolute;
+      left: -8px;
+      top: 3px;
+      background: linear-gradient( 180deg, #32DAF9 0%, #28C1F9 100%);
+    }
+  }
+  >ul{
+    li{
+      display: flex;
+      font-size: 12px;
+      margin-top: 10px;
+      line-height: 15px;
+      >.value{
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
--- src/views/AfterSales/ResponsibilityMeasureInfoPage.vue
+++ src/views/AfterSales/ResponsibilityMeasureInfoPage.vue
@@ -1,16 +1,29 @@
 <template>
-  <section class="mp-feedback-page-main-wrap">
-    bbbbbbbbbbbb
+  <section class="responsibility-measure-info-page-wrap">
+    <header>
+      aaa
+    </header>
+    <main>
+      <AfterSalesSolutionInfoComp/>
+      <ResponsibilityDifferentiationComp/>
+      <OrderDetailsComp/>
+    </main>
     <el-button @click="onGoBackClick">返回</el-button>
   </section>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import AfterSalesSolutionInfoComp from '@/components/AfterSalesComps/AfterSalesSolutionInfoComp.vue';
+import ResponsibilityDifferentiationComp from '@/components/AfterSalesComps/ResponsibilityDifferentiationComp.vue';
+import OrderDetailsComp from '@/components/AfterSalesComps/OrderDetailsComp.vue';
 
 export default {
   name: 'ResponsibilityMeasurePage',
   components: {
+    AfterSalesSolutionInfoComp,
+    ResponsibilityDifferentiationComp,
+    OrderDetailsComp,
   },
   data() {
     return {
@@ -38,5 +51,10 @@ export default {
 
 <style lang='scss'>
 @import "@/assets/css/var.scss";
-
+.responsibility-measure-info-page-wrap{
+  >main{
+    display: flex;
+    justify-content: flex-start;
+  }
+}
 </style>
