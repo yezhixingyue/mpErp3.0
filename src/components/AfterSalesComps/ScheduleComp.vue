f5be35d335e1a0954f3da207d8f2caacc4cb4c9a
lihaoyang <l15639757696@163.com>
2024-06-17 17:16:12 +0800

    nothing

--- src/components/AfterSalesComps/ScheduleComp.vue
+++ src/components/AfterSalesComps/ScheduleComp.vue
@@ -5,11 +5,50 @@
     </header>
     <ul>
       <li>
-        <p>2024-4-27 20:45</p>
+        <img src="@/assets/images/动线剪头.png" alt="">
+        <p>2024-4-27 20:42</p>
         <p title="李小二转给王大志">李小二转给王大志李小二转给王大志</p>
-        <p><span>查看原因</span></p>
+        <p>
+          <el-popover
+            placement="top-start"
+            title="标题"
+            width="200"
+            trigger="click"
+            content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <span slot="reference">查看原因</span>
+          </el-popover>
+        </p>
       </li>
-      <li></li>
+      <li>
+        <img src="@/assets/images/动线剪头.png" alt="">
+        <p>2024-4-27 20:42</p>
+        <p title="李小二转给王大志">李小二转给王大志李小二转给王大志</p>
+        <p>
+          <el-popover
+            placement="top-start"
+            title="标题"
+            width="200"
+            trigger="click"
+            content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <span slot="reference">查看原因</span>
+          </el-popover>
+        </p>
+      </li>
+      <li>
+        <img src="@/assets/images/动线剪头.png" alt="">
+        <p>2024-4-27 20:42</p>
+        <p title="李小二转给王大志">李小二转给王大志李小二转给王大志</p>
+        <p>
+          <el-popover
+            placement="top-start"
+            title="标题"
+            width="200"
+            trigger="click"
+            content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <span slot="reference">查看原因</span>
+          </el-popover>
+        </p>
+      </li>
     </ul>
   </section>
 </template>
@@ -43,16 +82,34 @@ export default {
     border-radius: 4px;
     display: flex;
     overflow-x: auto;
+    >li+li{
+      margin-left: 10px;
+    }
     >li{
       width: 124px;
       text-align: center;
       line-height: 16px;
       padding: 0 10px;
       box-sizing: border-box;
+      position: relative;
+      &:last-of-type{
+        >img{
+          filter: grayscale(0%);
+        }
+      }
+      >img{
+        position: absolute;
+        left: -10px;
+        top: 16px;
+        filter: grayscale(100%);
+      }
       >p{
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
+        >span{
+          color: #26BCF9;
+        }
       }
     }
   }
