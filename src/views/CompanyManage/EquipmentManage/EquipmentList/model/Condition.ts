c276bfc873ff60790bd79d48f2f17cf309442945
yezhixingyue <yezhixingyue@163.com>
2025-01-09 10:13:36 +0800

    办公设备列表增加传值

--- src/views/CompanyManage/EquipmentManage/EquipmentList/model/Condition.ts
+++ src/views/CompanyManage/EquipmentManage/EquipmentList/model/Condition.ts
@@ -23,24 +23,30 @@ export class Condition {
       if (typeof this.DepartmentID !== 'number' && !this.PostID) return null;
 
       let _staffList = staffList;
+      let screeningGroup = false; // 是否筛选部门或岗位
 
       if (typeof this.DepartmentID === 'number') {
         _staffList = _staffList.filter(staff => staff.PositionList.map(p => p.First.FirstDepartmentID).includes(this.DepartmentID as number));
+        screeningGroup = true;
       }
 
       if (this.PostID) {
         _staffList = _staffList.filter(staff => staff.PositionList.map(p => p.Second.PositionID).includes(this.PostID));
+        screeningGroup = true;
       }
 
-      return _staffList.map(staff => staff.StaffID);
+      return { staffIds: _staffList.map(staff => staff.StaffID), screeningGroup };
     };
+
+    const { staffIds, screeningGroup } = getStaffIds() || {};
 
     const temp = {
       ...this,
       status: this.Status || this.Status === 0 ? this.Status : undefined,
       type: this.Type || this.Type === 0 ? this.Type : undefined,
       keywords: this.Keywords || undefined,
-      staffIds: getStaffIds() || undefined,
+      staffIds: staffIds || undefined,
+      screeningGroup: screeningGroup || false, // 是否筛选部门或岗位
     };
 
     delete temp.Status;
